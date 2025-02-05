e86bf2cf10947c1c2c005865eaaa63bfb87ad5af
jadmax0w0 <jadmax@qq.com>
2025-02-05 10:05:45 +0800

    完善 UI 1. 微调 TitleCard, AccountItemList 的显示配置 2. 主控件改为 MainStack，嵌套先前的 MainColumn，并将 BottomRow 单独列出，使用 BottomRowContainer 实现模糊背景

--- ui.py
+++ ui.py
@@ -10,6 +10,7 @@ class UIConfig:
     BookTitleTextSize = 25
     BookTitleTextWeight = ft.FontWeight.W_600
 
+    TitleYearTextSize = 20
     TitleMonthTextSize = 15
     TitleMonthTextWight = ft.FontWeight.W_500
     TitleMonthTextWidth = 300
@@ -24,8 +25,8 @@ class UIConfig:
     TitleRowSpacing = 15
 
     TitleCardWidth = None
-    TitleCardHeight = 175
-    TitleCardInnerPadding = 20
+    TitleCardHeight = 185
+    TitleCardInnerPadding = ft.Padding(30, 15, 30, 15)
     
     # item list
     ItemTextSize = 15
@@ -34,20 +35,25 @@ class UIConfig:
     ItemNameWidth = 150
     ItemAmountWidth = 100
 
-    ItemListPadding = 15
+    ItemListPadding = ft.Padding(30, 0, 30, 0)
     ItemListSpacing = 15
     ItemListWidth = None
 
     # bottom bar
-    TypesListHeight = 30
+    TypesListHeight = 35
     TypesListSpacing = 15
-    TypeButtonWidth = 50
+    TypeButtonWidth = 65
     TypeButtonColor = ft.Colors.PRIMARY, ft.Colors.PRIMARY_CONTAINER
     TypeButtonColorSelected = ft.Colors.ON_PRIMARY, ft.Colors.ON_PRIMARY_CONTAINER
-    BottomRowWidth = None
-    BottomRowHeight = 60
     CreateItemButtonWidth = 60
     CreateItemButtonHeight = 60
+    BottomRowPadding = ft.Padding(10, 0, 10, 0)
+    BottomRowWidth = None
+    BottomRowHeight = 80
+    BottomRowBlur = 10
+
+    # main column
+    MainPadding = 15
 
 
 class UIMessage:
@@ -74,13 +80,15 @@ class TitleCard(ft.Card):
         self.backend = backend
 
         self.book_title = ft.Text(value=self.backend.current_book.name, text_align=ft.TextAlign.LEFT, size=UIConfig.BookTitleTextSize, weight=UIConfig.BookTitleTextWeight, expand=True)
+        self.year_info = ft.Text(value=f"{datetime.now().year} 年", text_align=ft.TextAlign.LEFT, size=UIConfig.TitleYearTextSize, weight=UIConfig.TitleMonthTextWight, expand=2)
+        self.filtered_types_info = ft.Text(text_align=ft.TextAlign.RIGHT, size=UIConfig.TitleYearTextSize, weight=UIConfig.TitleMonthTextWight, expand=3)
         self.month_info = ft.Text(value=f"{datetime.now().month} 月", text_align=ft.TextAlign.LEFT, size=UIConfig.TitleMonthTextSize, weight=UIConfig.TitleMonthTextWight, expand=UIConfig.TitleMonthTextExpand)
-        self.filtered_types_info = ft.Text(text_align=ft.TextAlign.RIGHT, size=UIConfig.TitleMonthTextSize, weight=UIConfig.TitleMonthTextWight, expand=True)
         self.month_inout = ft.Text(value=self.backend.inout_monthly(to_info=True), text_align=ft.TextAlign.RIGHT, size=UIConfig.TitleInoutTextSize, weight=UIConfig.TitleInoutTextWeight, expand=UIConfig.TitleInoutTextExpand)
         self.switch_books_button = ft.FloatingActionButton(icon=ft.Icons.BOOK, on_click=self.switch_book)
 
+        self.row_year = ft.Row(controls=[self.year_info, self.filtered_types_info], vertical_alignment=ft.CrossAxisAlignment.CENTER, spacing=UIConfig.TitleRowSpacing, expand=True)
         self.row_month = ft.Row(controls=[self.month_info, self.month_inout], vertical_alignment=ft.CrossAxisAlignment.CENTER, spacing=UIConfig.TitleRowSpacing, expand=True)
-        self.col_month = ft.Column(controls=[self.filtered_types_info, self.row_month], horizontal_alignment=ft.CrossAxisAlignment.END, spacing=15, expand=True)
+        self.col_month = ft.Column(controls=[self.row_year, self.row_month], horizontal_alignment=ft.CrossAxisAlignment.END, spacing=15, expand=True)
         self.row_bottom = ft.Row(controls=[self.col_month, self.switch_books_button], vertical_alignment=ft.CrossAxisAlignment.CENTER, spacing=15, expand=True)
 
         self.col_title = ft.Column(controls=[self.book_title, self.row_bottom], horizontal_alignment=ft.CrossAxisAlignment.START, expand=True)
@@ -274,23 +282,38 @@ class CreateItemButton(ft.FloatingActionButton):
         self.on_click = None  # TODO
 
 
-class BottomRow(ft.Row):
-    def __init__(self, backend: AccountingApp, controls = None, alignment = None, vertical_alignment = None, spacing = None, tight = None, wrap = None, run_spacing = None, run_alignment = None, scroll = None, auto_scroll = None, on_scroll_interval = None, on_scroll = None, ref = None, key = None, width = None, height = None, left = None, top = None, right = None, bottom = None, expand = None, expand_loose = None, col = None, opacity = None, rotate = None, scale = None, offset = None, aspect_ratio = None, animate_opacity = None, animate_size = None, animate_position = None, animate_rotation = None, animate_scale = None, animate_offset = None, on_animation_end = None, visible = None, disabled = None, data = None, rtl = None, adaptive = None):
-        super().__init__(controls, alignment, vertical_alignment, spacing, tight, wrap, run_spacing, run_alignment, scroll, auto_scroll, on_scroll_interval, on_scroll, ref, key, width, height, left, top, right, bottom, expand, expand_loose, col, opacity, rotate, scale, offset, aspect_ratio, animate_opacity, animate_size, animate_position, animate_rotation, animate_scale, animate_offset, on_animation_end, visible, disabled, data, rtl, adaptive)
+class BottomRowContainer(ft.Container):
+    def __init__(self, backend: AccountingApp, content = None, padding = None, margin = None, alignment = None, bgcolor = None, gradient = None, blend_mode = None, border = None, border_radius = None, image_src = None, image_src_base64 = None, image_repeat = None, image_fit = None, image_opacity = None, shape = None, clip_behavior = None, ink = None, image = None, ink_color = None, animate = None, blur = None, shadow = None, url = None, url_target = None, theme = None, theme_mode = None, color_filter = None, ignore_interactions = None, foreground_decoration = None, on_click = None, on_tap_down = None, on_long_press = None, on_hover = None, ref = None, key = None, width = None, height = None, left = None, top = None, right = None, bottom = None, expand = None, expand_loose = None, col = None, opacity = None, rotate = None, scale = None, offset = None, aspect_ratio = None, animate_opacity = None, animate_size = None, animate_position = None, animate_rotation = None, animate_scale = None, animate_offset = None, on_animation_end = None, tooltip = None, badge = None, visible = None, disabled = None, data = None, rtl = None, adaptive = None):
+        super().__init__(content, padding, margin, alignment, bgcolor, gradient, blend_mode, border, border_radius, image_src, image_src_base64, image_repeat, image_fit, image_opacity, shape, clip_behavior, ink, image, ink_color, animate, blur, shadow, url, url_target, theme, theme_mode, color_filter, ignore_interactions, foreground_decoration, on_click, on_tap_down, on_long_press, on_hover, ref, key, width, height, left, top, right, bottom, expand, expand_loose, col, opacity, rotate, scale, offset, aspect_ratio, animate_opacity, animate_size, animate_position, animate_rotation, animate_scale, animate_offset, on_animation_end, tooltip, badge, visible, disabled, data, rtl, adaptive)
         self.backend = backend
-
-        self.width = UIConfig.BottomRowWidth
-        self.height = UIConfig.BottomRowHeight
 
         self.item_types = ItemTypesList(self.backend)
         self.create_item_button = CreateItemButton(self.backend)
-        self.controls = [self.item_types, self.create_item_button]
+        self.row = ft.Row(controls=[self.item_types, self.create_item_button], width=UIConfig.BottomRowWidth, height=UIConfig.BottomRowHeight)
+
+        self.content = self.row
+        self.width = UIConfig.BottomRowWidth
+        self.height = UIConfig.BottomRowHeight
+        self.blur = UIConfig.BottomRowBlur
+        self.padding = UIConfig.BottomRowPadding
+        self.expand = True
     
     def update(self):
         super().update()
-        for c in self.controls:
-            c.update()
+        self.item_types.update()
+        self.create_item_button.update()
+        self.row.update()
         print(f"{self.__class__.__name__} updated")
+
+
+class BottomContainer(ft.Container):
+    def __init__(self, backend: AccountingApp, content = None, padding = None, margin = None, alignment = None, bgcolor = None, gradient = None, blend_mode = None, border = None, border_radius = None, image_src = None, image_src_base64 = None, image_repeat = None, image_fit = None, image_opacity = None, shape = None, clip_behavior = None, ink = None, image = None, ink_color = None, animate = None, blur = None, shadow = None, url = None, url_target = None, theme = None, theme_mode = None, color_filter = None, ignore_interactions = None, foreground_decoration = None, on_click = None, on_tap_down = None, on_long_press = None, on_hover = None, ref = None, key = None, width = None, height = None, left = None, top = None, right = None, bottom = None, expand = None, expand_loose = None, col = None, opacity = None, rotate = None, scale = None, offset = None, aspect_ratio = None, animate_opacity = None, animate_size = None, animate_position = None, animate_rotation = None, animate_scale = None, animate_offset = None, on_animation_end = None, tooltip = None, badge = None, visible = None, disabled = None, data = None, rtl = None, adaptive = None):
+        super().__init__(content, padding, margin, alignment, bgcolor, gradient, blend_mode, border, border_radius, image_src, image_src_base64, image_repeat, image_fit, image_opacity, shape, clip_behavior, ink, image, ink_color, animate, blur, shadow, url, url_target, theme, theme_mode, color_filter, ignore_interactions, foreground_decoration, on_click, on_tap_down, on_long_press, on_hover, ref, key, width, height, left, top, right, bottom, expand, expand_loose, col, opacity, rotate, scale, offset, aspect_ratio, animate_opacity, animate_size, animate_position, animate_rotation, animate_scale, animate_offset, on_animation_end, tooltip, badge, visible, disabled, data, rtl, adaptive)
+        self.backend = backend
+
+        self.row_content = BottomRowContainer(self.backend)
+        self.content = self.row_content
+        self.expand = True
 
 
 class MainColumn(ft.Column):
@@ -300,12 +323,29 @@ class MainColumn(ft.Column):
 
         self.title_card = TitleCard(backend=self.backend)
         self.items_list = AccountItemList(backend=self.backend)
-        self.bottom_bar = BottomRow(backend=self.backend)
-        self.controls = [self.title_card, self.items_list, self.bottom_bar]
+        self.controls = [self.title_card, self.items_list]
 
         self.expand=True
         self.spacing=UIConfig.ItemListSpacing
         self.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+
+    def update(self):
+        super().update()
+        for c in self.controls:
+            c.update()
+        print(f"{self.__class__.__name__} updated")
+
+
+class MainStack(ft.Stack):
+    def __init__(self, backend: AccountingApp, controls = None, clip_behavior = None, alignment = None, fit = None, ref = None, key = None, width = None, height = None, left = None, top = None, right = None, bottom = None, expand = None, expand_loose = None, col = None, opacity = None, rotate = None, scale = None, offset = None, aspect_ratio = None, animate_opacity = None, animate_size = None, animate_position = None, animate_rotation = None, animate_scale = None, animate_offset = None, on_animation_end = None, visible = None, disabled = None, data = None, adaptive = None):
+        super().__init__(controls, clip_behavior, alignment, fit, ref, key, width, height, left, top, right, bottom, expand, expand_loose, col, opacity, rotate, scale, offset, aspect_ratio, animate_opacity, animate_size, animate_position, animate_rotation, animate_scale, animate_offset, on_animation_end, visible, disabled, data, adaptive)
+        self.backend = backend
+
+        self.main_column = MainColumn(self.backend)
+        self.bottom_bar = BottomContainer(self.backend)
+        self.controls = [self.main_column, self.bottom_bar]
+        self.expand = True
+        self.alignment = ft.alignment.bottom_center
 
     def update(self):
         super().update()
@@ -320,13 +360,14 @@ class AccountingAppUI(ft.Container):
         super().__init__(controls, alignment, horizontal_alignment, spacing, tight, wrap, run_spacing, run_alignment, ref, key, width, height, left, top, right, bottom, expand, expand_loose, col, opacity, rotate, scale, offset, aspect_ratio, animate_opacity, animate_size, animate_position, animate_rotation, animate_scale, animate_offset, on_animation_end, visible, disabled, data, rtl, scroll, auto_scroll, on_scroll_interval, on_scroll, adaptive)
         self.backend = backend
 
-        self.main_column = MainColumn(self.backend)
+        # self.main_column = MainColumn(self.backend)
+        self.main_stack = MainStack(self.backend)
 
         # message subscriptions
-        self.main_column.bottom_bar.item_types.message_pub.add(self.main_column.items_list.filter_items)
-        self.main_column.items_list.message_pub.add(self.main_column.title_card.filtered_monthly_inout)
+        self.main_stack.bottom_bar.row_content.item_types.message_pub.add(self.main_stack.main_column.items_list.filter_items)
+        self.main_stack.main_column.items_list.message_pub.add(self.main_stack.main_column.title_card.filtered_monthly_inout)
         
-        self.content = self.main_column
+        self.content = self.main_stack
         self.expand = True
 
     def update(self):
